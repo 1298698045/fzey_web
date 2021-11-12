61a41b914d7fd3de8ce1e92ccdb6e5059ccbc41b
zd <1298698045@qq.com>
2021-11-12 20:45:48 +0800

    绑定首页模板

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ var mixin = {
             return (this.selectMenu || this.typePath)
         }
     },
-    mounted() {
+    mounted:function() {
         window.addEventListener('scroll',this.handleIsScroll,true)
     },
     methods: {
@@ -33,17 +33,17 @@ var mixin = {
         onMenuMouseLeave: function() {
             this.selectMenu = ''
         },
-        handleTabQrCode(idx){
+        handleTabQrCode:function(idx){
             this.qrCodeIdx = idx;
         },
-        handleIsScroll() {
+        handleIsScroll:function() {
             let scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
             scrolltop > 500 ? (this.isGotop = true) : (this.isGotop = false);
         },
-        toTop() {
+        toTop:function() {
             let top = document.documentElement.scrollTop || document.body.scrollTop;
             // 实现滚动效果 
-            const timeTop = setInterval(() => {
+            const timeTop = setInterval(function(){
                 document.body.scrollTop = document.documentElement.scrollTop = top -= 50;
                 if (top <= 0) {
                     this.isTop = true;
@@ -53,67 +53,67 @@ var mixin = {
                 }
             }, 10);
         },
-        handleOpen(){
+        handleOpen:function(){
             this.$refs.childModal.dialogVisible = true;
         }
     }
 }
 
-const Modal = 
-Vue.extend({
-    template: `
-        <el-dialog
-        title="试用申请咨询"
-        :visible.sync="dialogVisible"
-        width="440px"
-        :before-close="handleClose">
-        <div class="modalContent">
-            <div class="desc">完善以下信息您可以免费体验我们的产品功能，我们的顾问会在 1个工作日内与您取得联系</div>
-            <el-form ref="form" :rules="rules" :model="modalForm" label-width="80px">
-                <el-form-item required class="form_item" size="mini" label="单位名称" prop="unitName">
-                    <el-input v-model="modalForm.unitName"></el-input>
-                </el-form-item>
-                <el-form-item required class="form_item" size="mini" label="姓名" prop="name">
-                    <el-input v-model="modalForm.name"></el-input>
-                </el-form-item>
-                <el-form-item required class="form_item" size="mini" label="手机号" prop="phone">
-                    <el-input v-model="modalForm.phone"></el-input>
-                </el-form-item>
-            </el-form>
-        </div>
-        <span slot="footer" class="dialog-footer">
-            <el-button class="submitBtn" type="primary" @click="dialogVisible = false">提交申请</el-button>
-        </span>
-    </el-dialog>
-    `,
-    data(){
-        return {
-            dialogVisible:false,
-            modalForm:{
-                unitName: '',
-                name: '',
-                phone: ''
-            },
-            rules:{
-                unitName:[
-                    { required: true, message: '请输入单位名称', trigger: 'blur' }
-                ],
-                name:[
-                    { required: true, message: '请输入姓名', trigger: 'blur' }
-                ],
-                phone:[
-                    { required: true, message: '请输入电话', trigger: 'blur' }
-                ]
-            }
-        }
-    },
-    methods:{
-        handleClose(){
-            this.dialogVisible = false;
-        },
-        // 弹框
-        handleClose(){
-            this.dialogVisible = false;
-        }
-    }
-})
+// const Modal = 
+// Vue.extend({
+//     template: `
+//         <el-dialog
+//         title="试用申请咨询"
+//         :visible.sync="dialogVisible"
+//         width="440px"
+//         :before-close="handleClose">
+//         <div class="modalContent">
+//             <div class="desc">完善以下信息您可以免费体验我们的产品功能，我们的顾问会在 1个工作日内与您取得联系</div>
+//             <el-form ref="form" :rules="rules" :model="modalForm" label-width="80px">
+//                 <el-form-item required class="form_item" size="mini" label="单位名称" prop="unitName">
+//                     <el-input v-model="modalForm.unitName"></el-input>
+//                 </el-form-item>
+//                 <el-form-item required class="form_item" size="mini" label="姓名" prop="name">
+//                     <el-input v-model="modalForm.name"></el-input>
+//                 </el-form-item>
+//                 <el-form-item required class="form_item" size="mini" label="手机号" prop="phone">
+//                     <el-input v-model="modalForm.phone"></el-input>
+//                 </el-form-item>
+//             </el-form>
+//         </div>
+//         <span slot="footer" class="dialog-footer">
+//             <el-button class="submitBtn" type="primary" @click="dialogVisible = false">提交申请</el-button>
+//         </span>
+//     </el-dialog>
+//     `,
+//     data(){
+//         return {
+//             dialogVisible:false,
+//             modalForm:{
+//                 unitName: '',
+//                 name: '',
+//                 phone: ''
+//             },
+//             rules:{
+//                 unitName:[
+//                     { required: true, message: '请输入单位名称', trigger: 'blur' }
+//                 ],
+//                 name:[
+//                     { required: true, message: '请输入姓名', trigger: 'blur' }
+//                 ],
+//                 phone:[
+//                     { required: true, message: '请输入电话', trigger: 'blur' }
+//                 ]
+//             }
+//         }
+//     },
+//     methods:{
+//         handleClose(){
+//             this.dialogVisible = false;
+//         },
+//         // 弹框
+//         handleClose(){
+//             this.dialogVisible = false;
+//         }
+//     }
+// })
